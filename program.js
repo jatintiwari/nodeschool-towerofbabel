76eade6459fd4a7a3e7a99ef82286d49f8419aa1
jatintiwari <mail@jatintiwari.com>
2018-05-07 11:44:11 +0530

    inheritance

--- program.js
+++ program.js
@@ -1,26 +1,56 @@
-class Character{
+class Character {
 
-	constructor(x, y){
+	constructor(x, y) {
 		this.x = x;
 		this.y = y;
 		this.health_ = 100;
 	}
 
-	damage(){
+	attack(character) {
+		if (!(character instanceof Character)) throw new Error('Not a true character');
+		this.health_ -= 10;
+	}
+
+	damage() {
 		this.health_ = this.health_ - 10;
 	}
 
-	getHealth(){
-		 return this.health_;
+	getHealth() {
+		return this.health_;
 	}
 
-	toString(){
+	toString() {
 		return "x: " + this.x + " y: " + this.y + " health: " + this.getHealth();
 	}
 }
 
-var x = process.argv[2];
-var y = process.argv[3];
-var character = new Character(+x, +y);
-character.damage();
-console.log(character.toString());
+
+class Player extends Character {
+	constructor(x, y, name) {
+		super(x, y);
+		this.name = name;
+	}
+
+	move(dx, dy) {
+		this.x += dx;
+		this.y += dy;
+	}
+	toString() {
+		return "name: " + this.name + " " + super.toString.call(this);
+	}
+}
+
+try {
+	var x = process.argv[2];
+	var y = process.argv[3];
+	var name = process.argv[4];
+	var character = new Character(+x, +y);
+	character.damage();
+	console.log(character.toString());
+	var player = new Player(+x, +y, name);
+	player.damage();
+	player.move(7, 8);
+	console.log(player.toString());
+}catch(e){
+	console.error(`error: `,error.message);
+}
